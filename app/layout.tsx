f0cad674ceec619b225a79cd9eca085ac91f5b4a
pleinbob-arch <plein.bob@gmail.com>
2026-05-21 16:27:26 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,26 @@
-export const metadata = {
-  title: "Kondschafter Auktion",
-  description: "Online Auktion Weinfest Grevenmacher",
-};
+import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react'
+
+export const metadata: Metadata = {
+  title: 'Kondschafter Auktioun',
+  description: '76. Gréiwemaacher Drauwen- A Wäifest'
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="lb">
-      <body>{children}</body>
+      <body style={{
+        margin: 0,
+        padding: 0
+      }}>
+        {children}
+
+        <Analytics />
+      </body>
     </html>
-  );
+  )
 }
