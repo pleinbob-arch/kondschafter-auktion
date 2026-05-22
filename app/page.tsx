b81d3bc4a67499ebc789191bda3bf2855cfac630
pleinbob-arch <plein.bob@gmail.com>
2026-05-22 09:47:11 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -425,7 +425,7 @@ export default function Home() {
 
 <p>
   <strong>Kënschtler / Artist:</strong><br />
-  André Scholtes<br /><br />
+  André Scholtes<br />
   IT WAS NOT ME S.à r.l.<br />
   B276670<br />
   11, Rue des Tanneurs<br />
