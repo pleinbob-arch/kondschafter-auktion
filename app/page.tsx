b4cad32444948dfc5049966cc95cec3de8971adc
pleinbob-arch <plein.bob@gmail.com>
2026-05-22 17:39:56 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -567,7 +567,7 @@ export default function Home() {
     fontSize:'12px',
     opacity:0.82
   }}>
-    © 2026 Kondschafter - association sans but lucratif - Grevenmacher - All rights reserved.
+    © 2026 Kondschafter - association sans but lucratif - Grevenmacher - All rights reserved. ©
   </p>
 
 </footer>
