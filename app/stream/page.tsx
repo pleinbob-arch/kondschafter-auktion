97342ca464ea4f6f429245f92c76fdae4a934d21
pleinbob-arch <plein.bob@gmail.com>
2026-05-27 11:29:53 +0200

    Update page.tsx

--- app/stream/page.tsx
+++ app/stream/page.tsx
@@ -522,13 +522,14 @@ function Card({
   children: React.ReactNode
   centered?: boolean
 }) {
-  useEffect(() => {
-  const interval = setInterval(() => {
-    window.location.reload()
-  }, 1000 * 60 * 5)
+    useEffect(() => {
+    const interval = setInterval(() => {
+      window.location.reload()
+    }, 1000 * 60 * 5)
 
-  return () => clearInterval(interval)
-}, [])
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <div style={{
       background:'rgba(255,255,255,0.92)',
